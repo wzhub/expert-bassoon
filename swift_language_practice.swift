a5513aef83998803e4fb5f281e702dc0d5592323
wzhub <wenzhun@hotmail.com>
2025-05-07 19:19:44 +0800

    Create swift_language_practice.swift

---
+++ swift_language_practice.swift
@@ -0,0 +1,97 @@
+@propertyWrapper struct TwelveOrLess{private var number = 0;var wrappedValue:Int{get{return number}
+set{number = min(newValue,12)}}}
+struct SmallRectangle{
+ private var _width = TwelveOrLess(),_height = TwelveOrLess()
+ private var width:Int{
+  get{
+   return _width.wrappedValue
+  }
+  set{
+   _width.wrappedValue = newValue
+  }
+ }
+ private var height:Int{
+  get{
+   return _height.wrappedValue
+  }
+  set{
+   _height.wrappedValue = newValue
+  }
+ }
+}
+struct AnotherSmallRectangle{
+ @TwelveOrLess private var width:Int
+ @TwelveOrLess var height:Int
+}
+var rectangle = AnotherSmallRectangle()
+print(rectangle.height)
+rectangle.height = 10
+print(rectangle.height)
+rectangle.height = 24
+print(rectangle.height)
+@propertyWrapper 
+struct SmallNumber{
+ private var maximum:Int
+ private var number:Int
+ var wrappedValue:Int{
+  get{
+   return number
+  }
+  set{
+   number = min(
+    newValue,
+    maximum
+   )
+  }
+ }
+ init(){
+  maximum = 12
+  number = 0
+ }
+ init(wrappedValue:Int){
+  maximum = 12
+  number = min(wrappedValue,maximum)
+ }
+ init(wrappedValue:Int,maximum:Int){
+  self.maximum = maximum
+  number = min(wrappedValue,maximum)
+ }
+}
+struct ZeroRectangle{
+ @SmallNumber var width:Int
+ @SmallNumber var height:Int
+}
+let zeroRectangle = ZeroRectangle()
+print(
+ zeroRectangle.width,
+ zeroRectangle.height
+)
+struct UnitRectangle{
+ @SmallNumber var width:Int = 1
+ @SmallNumber var height:Int = 1
+}
+let unitRectangle = UnitRectangle()
+print(
+ unitRectangle.width,
+ unitRectangle.height
+)
+struct NarrowRectangle{
+ @SmallNumber(wrappedValue:2,maximum:5) var width:Int
+ @SmallNumber(wrappedValue:3,maximum:4) var height:Int
+}
+let narrowRectangle = NarrowRectangle()
+print(
+ narrowRectangle.width,
+ narrowRectangle.height
+)
+struct MixedRectangle{
+ @SmallNumber(maximum:5) var width:Int = 9
+ @SmallNumber var height:Int = 10
+}
+var mixedRectangle = MixedRectangle()
+print(
+ mixedRectangle.width,
+ mixedRectangle.height
+)
+mixedRectangle.height = 20
+print(mixedRectangle.height)
