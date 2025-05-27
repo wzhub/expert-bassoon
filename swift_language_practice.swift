ec6b3663d2c9288823cd9bc5385c5e5f77896e08
wzhub <wenzhun@hotmail.com>
2025-05-27 19:28:51 +0800

    Add files via upload

--- swift_language_practice.swift
+++ swift_language_practice.swift
@@ -1,46 +1,31 @@
-@propertyWrapper struct SmallNumber{private var number:Int,maximum:Int;var wrappedValue:Int{get{return number}
-set{number = min(newValue,maximum)}};init(){maximum = 12;number = 0};init(wrappedValue:Int){maximum = 12;number = min(wrappedValue,maximum)};init(wrappedValue:Int,maximum:Int){self.maximum = maximum;number = min(wrappedValue,maximum)}};struct ZeroRectangle{@SmallNumber var width:Int;@SmallNumber var height:Int};let zeroRectangle = ZeroRectangle();print(zeroRectangle.width,zeroRectangle.height)
-struct UnitRectangle{
- @SmallNumber var width:Int = 1
- @SmallNumber var height:Int = 1
+@propertyWrapper
+struct SmallNumber {
+    private var number: Int
+    private let maximum: Int
+
+    var wrappedValue: Int {
+        get { number }
+        set { number = min(newValue, maximum) }
+    }
+
+    init(wrappedValue: Int, maximum: Int = 12) {
+        self.maximum = maximum
+        self.number = min(wrappedValue, maximum)
+    }
 }
-let unitRectangle = UnitRectangle()
-print(
- unitRectangle.width,
- unitRectangle.height
-)
-struct NarrowRectangle{
- @SmallNumber(
-  wrappedValue:2,
-  maximum:5
- ) var width:Int
- @SmallNumber(
-  wrappedValue:3,
-  maximum:4
- ) var height:Int
+
+struct MixedRectangle {
+    @SmallNumber(maximum: 10) var width = 5
+    @SmallNumber var height = 9
 }
-let narrowRectangle = NarrowRectangle()
-print(
- narrowRectangle.width,
- narrowRectangle.height
-)
-struct MixedRectangle{
- @SmallNumber(maximum:10) var width:Int = 5
- @SmallNumber var height:Int = 9
-}
+
 var mixedRectangle = MixedRectangle()
-print(
- mixedRectangle.width,
- mixedRectangle.height
-)
+print(mixedRectangle.width, mixedRectangle.height)
+
 mixedRectangle.width = 20
 mixedRectangle.height = 20
-print(
- mixedRectangle.width,
- mixedRectangle.height
-)
-@propertyWrapper
-struct AnotherSmallNumber{
+print(mixedRectangle.width, mixedRectangle.height)
+@propertyWrapper struct AnotherSmallNumber{
  private var number:Int
  private(set) var projectedValue:Bool
  var wrappedValue:Int{
@@ -66,11 +51,17 @@ struct SmallRectangle{
  @AnotherSmallNumber private var width:Int;@AnotherSmallNumber var height:Int
 }
 var rectangle = SmallRectangle()
-print(rectangle.height)
+print(
+ rectangle.height
+)
 rectangle.height = 10
-print(rectangle.height)
+print(
+ rectangle.height
+)
 rectangle.height = 24
-print(rectangle.height)
+print(
+ rectangle.height
+)
 struct SomeStruct{
  @AnotherSmallNumber var someNumber:Int
 }
@@ -80,20 +71,20 @@ print(someStruct.$someNumber)
 someStruct.someNumber = 55
 print(someStruct.$someNumber)
 enum Size{
- case small
  case large
+ case small
 }
 struct SizedRectangle{
  @AnotherSmallNumber private var width:Int
  @AnotherSmallNumber private var height:Int
  mutating func resize(to size:Size)->Bool{
   switch size{
-   case .small:
-    width = 10
-    height = 10
    case .large:
     width = 100
     height = 100
+   case .small:
+    width = 10
+    height = 10
   }
   return $width || $height
  }
