21310b13730047cb6bb3d03955b6dc45cdc210b5
wzhub <wenzhun@hotmail.com>
2025-06-08 23:11:52 +0800

    Add files via upload

--- swift_language_practice.swift
+++ swift_language_practice.swift
@@ -1,48 +1,6 @@
 @propertyWrapper struct SmallNumber{private var number:Int;private(set) var projectedValue:Bool;var wrappedValue:Int{get{number}
-set{if newValue > 12{number = 12;projectedValue = true}else{number = newValue;projectedValue = false}}};init(){self.number = 0;self.projectedValue = false}};struct SmallRectangle{@SmallNumber var height:Int};var rectangle = SmallRectangle()
-print(rectangle.height)
-rectangle.height = 10
-print(
- rectangle.height
-)
-rectangle.height = 24
-print(
- rectangle.height
-)
-struct SomeStruct{
- @SmallNumber var someNumber:Int
-}
-var someStruct = SomeStruct()
-someStruct.someNumber = 5
-print(
- someStruct.$someNumber
-)
-someStruct.someNumber = 55
-print(
- someStruct.$someNumber
-)
-enum Size{
- case large
- case small
-}
-struct SizedRectangle{
- @SmallNumber private var width:Int
- @SmallNumber private var height:Int
- mutating func resize(
-  to size:Size
- )->Bool{
-  switch size{
-   case .large:
-    width = 100
-    height = 100
-   case .small:
-    width = 10
-    height = 10
-  }
-  return $width || $height
- }
-}
-func someFunction(){
+set{if newValue > 12{number = 12;projectedValue = true}else{number = newValue;projectedValue = false}}};init(){self.number = 0;self.projectedValue = false}};enum Size{case large,small};struct SizedRectangle{@SmallNumber private var width:Int;@SmallNumber private var height:Int;mutating func resize(to size:Size)->Bool{switch size{case .large:width = 100;height = 100;case .small:width = 10;height = 10};return $width || $height}}
+func someFunc(){
  @SmallNumber var myNumber:Int
  myNumber = 10
  print(
@@ -54,10 +12,34 @@ func someFunction(){
  )
 }
 struct SomeStructure{
- static let storedTypeProperty = "some value"
- static var computedTypeProperty:Int{
+ static let storedTypeProperty:String = "some value"
+ static private var computedTypeProperty:Int{
   return 0
  }
 }
 enum SomeEnumeration{
+  static private let storedTypeProperty:String = "some value"
+ static var computedTypeProperty:Int{
+  return 1
+ }
+}
+class SomeClass{
+  static private let storedTypeProperty:String = "some value"
+ static var computedTypeProperty:Int{
+  return 2
+ }
+ class var overridableComputedTypeProperty:Int{
+  return 3
+ }
+}
+print(
+ SomeStructure.storedTypeProperty
+)
+print(
+ SomeEnumeration.computedTypeProperty
+)
+print(
+ SomeClass.computedTypeProperty
+)
+struct AudioChannel{
 }
